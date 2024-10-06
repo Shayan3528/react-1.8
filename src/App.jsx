bbddf9bd0ef55760ade544031fce9a1e38ddbc7a
Shayan <35280babu@gmail.com>
2024-10-06 18:18:16 +0600

    Rendering list

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,107 @@
-// import getImageUrl from "./component/Avatar";
-import Profile from "./component/Profile";
+
+/*
+
+import { person } from './component//Data.jsx';
+import { getImageUrl} from './component/Utlis.jsx';
+
+export default function App() {
+
+  const chemistry = person.filter(person => person.profession==='chemist');
+  const everyoneElse = person.filter(person => person.profession !=='chemist');
 
 
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemists</h2>
+      <ul>
+        {chemistry.map( person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+      <h2>Everyone Else</h2>
+      <ul>
+        {everyoneElse.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+    </article>
+  );  
+
+
+
+}
+
+*/
+
+import {recipes} from './component/Data.jsx';
+/*
 export default function App(){
-    return (
+  return (
+    <div>
+      <h1>Recipes</h1>
+      { recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map(ingredient => (
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+*/
+
+  function Recipe({id,name,ingredients}){
+  return(
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map(ingredients => 
+          <li key = {ingredients}>
+            {ingredients}
+
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+
+}
+  export default function App(){
+    return(
       <div>
-        <h1>Notable Scientists</h1>
-        <Profile
-          imageId="szV5sdG"
-          name="Maria Skłodowska-Curie"
-          profession="physicist and chemist"
-          discovery="polonium (chemical element)"
-          awards={[
-            'Nobel Prize in Physics',
-            'Nobel Prize in Chemistry',
-            'Davy Medal',
-            'Matteucci Medal'
-          ]}
-        />
-        <Profile
-          imageId='YfeOqp2'
-          name='Katsuko Saruhashi'
-          profession='geochemist'
-          discovery="a method for measuring carbon dioxide in seawater"
-          awards={[
-            'Miyake Prize for geochemistry',
-            'Tanaka Prize'
-          ]}
-        />
+        <h1>Recipe</h1>
+        {recipes.map(recipes => 
+          <Recipe {...recipes} key = {recipes.id} />
+        )}
       </div>
-    );
+    )
   }
+
